c86b776f4558cc74a8339bc08d2c66f9ec3910a1
Finn Melzer <github@mfinn.de>
2021-11-18 16:29:45 +0100

    Fixed script for private repos

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ read PAT
 
 docker network create gitdeploy
 
-if [ -z "${VAR}" ]; then
+if [ -z "${PAT}" ]; then
   docker run -e REPOSITORY=$REPO \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v gitdeploy:/app/cloned-repo/ \
@@ -21,4 +21,6 @@ else
   f1nnm/git-docker-manager:release-0.5.28 init
 fi
 
+echo $REPO
+echo $PAT
 echo "Setup finished!"
